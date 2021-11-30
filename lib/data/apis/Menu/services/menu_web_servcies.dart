f37e312e796214eb9d3ba66e0ea96a523830b044
Mahmoud salah eldien saber <mahmoudsalah37@gmail.com>
2021-11-30 10:51:18 +0200

    abstract menu

--- lib/data/apis/Menu/services/menu_web_servcies.dart
+++ lib/data/apis/Menu/services/menu_web_servcies.dart
@@ -2,9 +2,9 @@ import 'package:linktsp_api/data/apis/Menu/models/menu_model.dart';
 import 'package:linktsp_api/data/apis/default_api.dart';
 import 'package:linktsp_api/data/apis/result_model.dart';
 
-class MenuWebServices extends DefaultApi {
-  MenuWebServices({String defaultPath = ''}) : super(defaultPath);
-
+class MenuWebServicesImp extends DefaultApi implements MenuWebServices {
+  MenuWebServicesImp({String defaultPath = ''}) : super(defaultPath);
+  @override
   Future<MenuModel> getMenu({required int customerID, int lang = 1}) async {
     final respose = await getData(
       path: 'home/menu',
@@ -21,7 +21,15 @@ class MenuWebServices extends DefaultApi {
     }
   }
 
+  @override
   Future getContactInfo() async {}
-
+  @override
   Future getPreOrderPolicy() async {}
+}
+
+abstract class MenuWebServices {
+  Future<MenuModel> getMenu({required int customerID, int lang = 1});
+  Future getContactInfo();
+
+  Future getPreOrderPolicy();
 }
