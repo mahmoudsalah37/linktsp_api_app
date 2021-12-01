b223a8315679be2d14411cb0124a58b6d34bc333
Abd Elrahman Ayman <abdelrahmanaymanfcis@gmail.com>
2021-12-01 11:13:49 +0200

    add review test

--- test/data/page_block/page_block_service_test.dart
+++ test/data/page_block/page_block_service_test.dart
@@ -6,7 +6,7 @@ import '../../const.dart';
 void main() async {
   await LinkTspApi.init(domin: domin);
   final LinkTspApi linkTspApi = LinkTspApi();
-  group('page block service: ', () {
+  group('page block service test: ', () {
     test('check page block length', () async {
       final result = await linkTspApi.pageBlock.getPageBlock();
       expect(result.length, 8);
