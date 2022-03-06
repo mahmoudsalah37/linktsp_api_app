02274ba515a4a4ec6c5a51f7d05ceb2632b14be8
OmarzSelim <39450674+OmarzSelim@users.noreply.github.com>
2022-03-06 14:36:23 +0200

    feature model

--- lib/core/models/feature_model.dart
+++ lib/core/models/feature_model.dart
@@ -3,13 +3,41 @@ class Feature {
     this.name,
     this.value,
     this.imageUrl,
+    this.values,
+  });
+
+  String? name;
+  String? value;
+  String? imageUrl;
+  List<FeatureValue>? values;
+
+  factory Feature.fromJson(Map<String, dynamic> json) => Feature(
+        name: json["name"],
+        value: json["value"],
+        imageUrl: json["imageURL"],
+        values: json["values"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+        "value": value,
+        "imageURL": imageUrl,
+        "values": values,
+      };
+}
+
+class FeatureValue {
+  FeatureValue({
+    this.name,
+    this.value,
+    this.imageUrl,
   });
 
   String? name;
   String? value;
   String? imageUrl;
 
-  factory Feature.fromJson(Map<String, dynamic> json) => Feature(
+  factory FeatureValue.fromJson(Map<String, dynamic> json) => FeatureValue(
         name: json["name"],
         value: json["value"],
         imageUrl: json["imageURL"],
