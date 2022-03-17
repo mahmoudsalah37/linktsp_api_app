239c4c0b848da199bb87b46d355204bf710588e6
Mahmoud salah eldien saber <mahmoudsalah37@gmail.com>
2022-03-17 13:02:49 +0200

    update check out review

--- lib/data/checkout/services/checkout_services.dart
+++ lib/data/checkout/services/checkout_services.dart
@@ -126,7 +126,7 @@ class CheckOutServicesImp implements CheckOutService {
   Future<CheckoutReviewModel> checkoutReview(
       {required int customerId,
       int? paymentOptionId,
-      int? addressId,
+      required int addressId,
       int? loyaltyPoints,
       int? pickStoreID,
       required String shipmentMethods}) async {
@@ -376,7 +376,7 @@ abstract class CheckOutService {
   Future<CheckoutReviewModel> checkoutReview(
       {required int customerId,
       int? paymentOptionId,
-      int? addressId,
+      required int addressId,
       int? loyaltyPoints,
       int? pickStoreID,
       required String shipmentMethods});
